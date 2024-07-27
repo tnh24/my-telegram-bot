16b9966f548df0ba688482f6b29ba28871572f51
ThetNaungHset <thetnaunghset@gmail.com>
2024-07-27 18:57:05 +0630

    add

--- netify/functons/setWebhook.js
+++ netify/functons/setWebhook.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 const TELEGRAM_BOT_TOKEN = '7382843737:AAE8nivAOxYJSF-a1JmSYt59g_lyqTKhTr8'; // Replace with your bot token
-const NETLIFY_FUNCTION_URL = 'https://ytscribe.netlify.app/'; // Replace with your Netlify function URL
+const NETLIFY_FUNCTION_URL = 'https://ytscribe.netlify.app/.netlify/functions/telegram-bot'; // Replace with your Netlify function URL
 
 axios.post(`https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/setWebhook`, {
   url: NETLIFY_FUNCTION_URL
